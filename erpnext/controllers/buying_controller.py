4f451faa9547e2f8efc0822487867ce06bc55d79
Datta Mundada <dbmundada333@gmail.com>
2024-07-02 18:59:57 +0530

    Update buying_controller.py

--- erpnext/controllers/buying_controller.py
+++ erpnext/controllers/buying_controller.py
@@ -16,6 +16,7 @@ from erpnext.controllers.subcontracting_controller import SubcontractingControll
 from erpnext.stock.get_item_details import get_conversion_factor
 from erpnext.stock.utils import get_incoming_rate
 
+
 class QtyMismatchError(ValidationError):
 	pass
 
